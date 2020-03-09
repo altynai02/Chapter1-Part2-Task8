63a307ef1a27ecd06d7a0f0f10532f1b81a11763
altynai <altynay.bakytbekova.02@gmal.com>
2020-03-09 23:30:48 +0600

    added

---
+++ task8.py
@@ -0,0 +1,8 @@
+# 8. Given two integers A and B (A ≤ B). 
+# Print all numbers from A to B
+# inclusively.
+
+integer_A = int(input("Enter the first number: "))
+integer_B = int(input("Enter the second number: "))
+for i in range(integer_A,integer_B):
+    print(i)
